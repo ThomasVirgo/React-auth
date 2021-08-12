dc2d8f93cd61147867e2c61f2cc08aef00ebb442
Tom Virgo <tomcvirgo@gmail.com>
2021-08-12 13:21:27 +0100

    add login and register routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { Switch, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import { Login, Register } from './pages';
 
@@ -6,8 +7,14 @@ const App = () => {
 
     return (
         <>
-        <Login/>
-        <Register/>
+        <Switch>
+            <Route exact path='/login'>
+                <Login/>
+            </Route>
+            <Route exact path='/register'>
+                <Register/>
+            </Route>
+        </Switch>
         </>
     )
 }
